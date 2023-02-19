ef2146da34de697df546ad82953b8d91416a8d5a
Lucas Vieira Brito <vieiralucas2205@gmail.com>
2023-02-19 01:06:23 -0300

    Responsividade navbar finalizada

--- JS/navbar.js
+++ JS/navbar.js
@@ -4,5 +4,42 @@ class Navbar {
     this.navList = document.querySelector(navList);
     this.navLinks = document.querySelectorAll(navLinks);
     this.activeClass = "active";
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.hamburguerMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    console.log(this);
+    this.hamburguerMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.hamburguerMenu) {
+      this.addClickEvent();
+    }
+    return this;
   }
 }
+
+const mobileNavbar = new Navbar(
+  ".menu-hamburguer",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
+console.log("lucas");
